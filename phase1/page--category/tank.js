edcafbbfa01be9588b04f099bc1ca7e120fc15e7
Richard-HC-HK <32241247+Richard-HC-HK@users.noreply.github.com>
2017-09-27 11:23:21 +0800

    Add files via upload

---
+++ phase1/page--category/tank.js
@@ -0,0 +1,41 @@
+function alistarFunction() {
+    a = document.getElementById("item1").value;
+    var a1 = parseInt(a);
+    a1 += 1;
+    document.getElementById("item1").value = a1.toString();
+}
+
+function dariusFunction(){
+  b = document.getElementById("item2").value;
+  var b1 = parseInt(b);
+  b1 += 1;
+  document.getElementById("item2").value = b1.toString();
+}
+
+function garenFunction(){
+  c = document.getElementById("item3").value;
+  var c1 = parseInt(c);
+  c1 += 1;
+  document.getElementById("item3").value = c1.toString();
+}
+
+function malphiteFunction(){
+  d = document.getElementById("item4").value;
+  var d1 = parseInt(d);
+  d1 += 1;
+  document.getElementById("item4").value = d1.toString();
+}
+
+function nautilusFunction(){
+  e = document.getElementById("item5").value;
+  var e1 = parseInt(e);
+  e1 += 1;
+  document.getElementById("item5").value = e1.toString();
+}
+
+function renektonFunction(){
+  f = document.getElementById("item6").value;
+  var f1 = parseInt(f);
+  f1 += 1;
+  document.getElementById("item6").value = f1.toString();
+}
